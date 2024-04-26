83c5d55181fe089ba91c6ca23696af2b4dc3fa4e
ADANFLORES34 <adanflores0506@gmail.com>
2024-04-26 14:23:24 -0700

    Primer Cambio

--- EntradasControladas/src/App.jsx
+++ EntradasControladas/src/App.jsx
@@ -1,35 +1,41 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+import React from 'react'
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+
+function App() {
+   const [Entradas,setEntradas] = useState([])
+
+
+    
+  const handleText = async(term) =>{
+
+  
+  
+  
+   
+    console.log(result)
+  
+    setEntradas(result)
+
+  }
+
+   
+
+    return (
+        <div>
+          <div>
+            Capture su texto
+          </div>
+          <input onChange={handleText} value={term}/>
+        </form>
+          <h3>Su Texto</h3>
+          <p>
+            {Entradas}
+          </p>
+        </div>
+    );
 }
 
-export default App
+export default App;
---
+++ EntradasControladas/src/Texto.jsx
@@ -0,0 +1,26 @@
+import App from './App.jsx'
+
+function Texto  {
+
+    const handleChange = (event) => {
+        console.log(event.target.value)
+        setTerm(event.target.value)
+    }
+
+
+        const handleClick = () => {
+            onSubmit(term)
+       }
+
+
+
+
+       
+}
+
+
+
+
+
+
+export default Texto;
